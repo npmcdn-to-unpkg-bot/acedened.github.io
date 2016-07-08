0084681a9080789c573bcfc5226e3090b1e0061d
Vadim Novoseltsev <acedened@outlook.com>
2016-07-09 01:05:13 +0300

    Upload website

---
+++ js/main.js
@@ -0,0 +1,75 @@
+window.onload = function() {
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            input: '',
+            history: [],
+            availableCommands: ['about', 'projects', 'links', 'help', 'clear'],
+            lines: []
+        },
+
+        methods: {
+            write: function(text, prompted, raw) {
+                if (!raw)
+                    text = text.replace(/ /g, '\u00a0')
+                this.lines.push({
+                    text: text,
+                    isPrompted: prompted
+                })
+
+                document.body.scrollTop = document.body.scrollHeight
+            },
+
+            execute: function(command) {
+                if (this.availableCommands.indexOf(command) == -1) {
+                    if (command.length > 0)
+                        this.write("Unknown command: " + command)
+                    return
+                }
+
+                this[command]()
+            },
+
+            about: function() {
+                this.write('Vadim Novoseltsev (acedened)')
+                this.write('Software developer')
+                this.write('C# (UWP, WPF, WinForms)')
+            },
+
+            projects: function() {
+                this.write('<a href="#">The Chan</a>', false, true)
+                this.write('<a href="#">clojure-vk</a>', false, true)
+            },
+
+            links: function() {
+                this.write('<a href="#">GitHub</a>', false, true)
+                this.write('<a href="#">Contact me</a>', false, true)
+            },
+
+            help: function() {
+                this.write('Available commands: about, projects, links, help, clear')
+            },
+
+            clear: function() {
+                this.lines = []
+            },
+
+            enter: function() {
+                this.writeAndExecute(this.input)
+                this.input = ""
+            },
+
+            writeAndExecute: function(command) {
+                this.history.push(command)
+                this.write(command, true)
+                this.execute(command.trim())
+            },
+
+            previousCommand: function() {
+                if (this.history.length > 0)
+                    this.input = this.history.pop()
+            }
+        }
+    })
+    vm.writeAndExecute('help')
+}
